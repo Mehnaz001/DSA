80323a3c9aece141304b332f884cade96637854e
Mehnaz001 <mehnaz62844@gmail.com>
2024-05-01 06:43:55 +0530

    Program to print Fibonacci Series

---
+++ FibonacciSeries.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+    int n;
+    cout<<"Enter number: ";
+    cin>>n;
+
+    int a=0,b=1;
+    for(int i=1; i<=n; i++){
+        int nextnum=a+b;
+        cout<<nextnum;
+        a=b;
+        b=nextnum;
+    }
+}
