53eb9d843a91514640faefb760f5c78fa2d7ec59
Mehnaz001 <mehnaz62844@gmail.com>
2024-05-01 06:50:57 +0530

    Program to check whether is prime or not

---
+++ PrimeorNot.cpp
@@ -0,0 +1,23 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+    bool isPrime=1;
+    int number;
+    cout<<"Enter a number: ";
+    cin>>number;
+
+    for(int i=2; i<=n; i++){
+        if(n%i==0){
+            isPrime=0;
+            break;
+        }
+    }
+
+    if(isPrime==0){
+        cout<<"Not a prime number.";
+    }
+    else{
+        cout<<"Prime number.";
+    }
+}
