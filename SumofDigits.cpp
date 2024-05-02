565b8cecbcb70073e635e4bd84764c7fa2371e79
Mehnaz001 <mehnaz62844@gmail.com>
2024-05-02 06:55:23 +0530

    Program to subtract the product and sum of the digits of integer

---
+++ SumofDigits.cpp
@@ -0,0 +1,15 @@
+//Subtract the product and sum of the digits of an integer
+#include<iostream>
+using namespace std;
+
+int main(){
+    int product=1,sum=0;
+    while(n){
+        product=product*(n%10);
+        sum=sum+(n%10);
+        n=n/10;
+    }
+    cout<<"product= "<<product;
+    cout<<"sum = "<<sum;
+    cout<<"product-sum = "<<product-sum;
+}
