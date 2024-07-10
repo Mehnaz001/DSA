ccb5797878d15dedbacff76d45a44eb312dfc9a0
Mehnaz001 <mehnaz62844@gmail.com>
2024-07-10 12:37:46 +0530

    Factorial of a Number

---
+++ Recursion/FactorialofNumber.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+using namespace std;
+
+int Fact(int n)
+{   if(n==1)
+    return 1;
+    return n*Fact(n-1);
+}
+
+int main(){
+    int n;
+    cout<<"Enter a number: ";
+    cin>>n;
+
+    cout<<Fact(n);
+}
