c83ed4284b91765e58f68047bd4634823e6551a5
Mehnaz001 <mehnaz62844@gmail.com>
2024-07-10 21:22:11 +0530

    Power of 2

---
+++ Recursion/powerof2.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+using namespace std;
+
+int pow(int num, int n)
+{   if(n==0)
+    return 1;
+    if(n==1)
+    return num;
+
+    return num*pow(num,n-1);
+}
+int main()
+{
+    int n;
+    cin>>n;
+
+    pow(2,n);
+}
