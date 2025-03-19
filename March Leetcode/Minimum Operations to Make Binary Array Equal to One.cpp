dada3d28ffc5f0c7924d110ecf72887a584af144
Mehnaz001 <165507214+Mehnaz001@users.noreply.github.com>
2025-03-19 22:55:51 +0530

    Create Minimum Operations to Make Binary Array Equal to One.cpp

---
+++ March Leetcode/Minimum Operations to Make Binary Array Equal to One.cpp
@@ -0,0 +1,31 @@
+class Solution {
+public:
+    int minOperations(vector<int>& nums) {
+        int ans = 0;
+        for(int i=0; i+2<nums.size(); i++) {
+            if(nums[i]==0 ) {
+                nums[i] = 1;
+                if(nums[i+1]==0) {
+                        nums[i+1] = 1;
+                }
+                else {
+                        nums[i+1] = 0;
+                }
+
+                if(nums[i+2]==0) {
+                        nums[i+2] = 1;
+                }
+                else {
+                        nums[i+2] = 0;
+                }
+
+                ans++;
+            }
+        }
+        for(int i=0; i<nums.size(); i++) {
+            if(nums[i]==0)
+            return -1;
+        }
+        return ans;
+    }
+};
